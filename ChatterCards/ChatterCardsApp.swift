49dee6d2e2e2a013ebae64bee533d8728a4e71e9
Nikhil Barik <nklmarch17@gmail.com>
2024-06-18 15:58:38 +0530

    Initial Commit

---
+++ ChatterCards/ChatterCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatterCardsApp.swift
+//  ChatterCards
+//
+//  Created by Nikhil Barik on 18/06/24.
+//
+
+import SwiftUI
+
+@main
+struct ChatterCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatterCards/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChatterCards
+//
+//  Created by Nikhil Barik on 18/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
